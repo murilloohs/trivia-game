154541a20bb77fa7d6f27cddae934fa893329b99
Brunapessoa <brunaa.m@hotmail.com>
2023-02-13 11:42:19 -0300

    Requisito 14

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from './Header';
 
-export default class Feedback extends Component {
+class Feedback extends Component {
   render() {
+    const { score, assertions } = this.props;
+
     return (
       <div>
         <section>
           <Header />
         </section>
         <div data-testid="feedback-text" />
+        <div data-testid="feedback-total-score">
+          { score }
+        </div>
+        <div data-testid="feedback-total-question">
+          { assertions }
+        </div>
       </div>
     );
   }
 }
+
+const mapStateToProps = (store) => ({
+  score: store.player.score,
+  assertions: store.player.assertions,
+});
+
+Feedback.propTypes = {
+  score: PropTypes.number.isRequired,
+  assertions: PropTypes.number.isRequired,
+};
+
+export default connect(mapStateToProps)(Feedback);
