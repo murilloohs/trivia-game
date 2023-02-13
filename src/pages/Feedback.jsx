416547011a8e17f9b66519c542cb16f3f17a33b0
Artur Henrique <arthenrdev@gmail.com>
2023-02-13 14:23:59 -0300

    Requisito 15

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -4,6 +4,11 @@ import PropTypes from 'prop-types';
 import Header from './Header';
 
 class Feedback extends Component {
+  redirectInitialPage = () => {
+    const { history: { push } } = this.props;
+    push('/');
+  };
+
   render() {
     const { score, assertions } = this.props;
 
@@ -19,6 +24,14 @@ class Feedback extends Component {
         <div data-testid="feedback-total-question">
           { assertions }
         </div>
+        <section>
+          <button
+            data-testid="btn-play-again"
+            onClick={ this.redirectInitialPage }
+          >
+            Play Again
+          </button>
+        </section>
       </div>
     );
   }
@@ -32,6 +45,9 @@ const mapStateToProps = (store) => ({
 Feedback.propTypes = {
   score: PropTypes.number.isRequired,
   assertions: PropTypes.number.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
 export default connect(mapStateToProps)(Feedback);
